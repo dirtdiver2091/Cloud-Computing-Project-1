2c3c43f41e346b189079518c08b53cef58496521
Andrew Pidhajny <tuh20780@temple.edu>
2021-03-14 12:00:59 -0400

    Added some imports and a few lines of code to the views file

--- views.py
+++ views.py
@@ -1,13 +1,26 @@
+import config
+import os
+import uuid
+
+from s3_operations import list_content, upload
+from flask import Flask, request, render_template, redirect
 from PIL import Image, ImageOps, ImageFilter
+
+views = Flask(__name__)
 
 def applyFilter(filename, filter):
 
+    # Retrieve image here
     image = 'C:\Users\Andrew Pidhajny\Temple Classes\Senior Year\Spring Semester\Cloud Computing\Project 1\Code\Cloud-Computing-Project-1\Images' + filename
 
     i = image.split('.')
     changedImageName = i[0] + '-changed.jpg'
 
+    # The edited image
     changedImage = 'C:\Users\Andrew Pidhajny\Temple Classes\Senior Year\Spring Semester\Cloud Computing\Project 1\Code\Cloud-Computing-Project-1\Changed Images' + changedImageName
+
+    # Save image here
+    im = Image.open(image)
 
     if filter == 'gray':
         im = ImageOps.grayscale(image)
@@ -19,3 +32,6 @@ def applyFilter(filename, filter):
     im.save(changedImage)
 
     return changedImageName
+
+if __name__ == "__main__":
+	views.run(debug=True)
