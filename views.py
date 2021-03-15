6b21fd847984617e37c6e204a797224710fbea5c
Andrew Pidhajny <tuh20780@temple.edu>
2021-03-14 22:05:44 -0400

    changed code to make server work. There is currently a bug that occurs when an image is to be filtered

--- views.py
+++ views.py
@@ -2,71 +2,70 @@ import config
 import os
 import uuid
 
-from s3_handeling import list_content, upload
+from s3_handling import list_content, upload
 from flask import Flask, request, render_template, redirect
 from PIL import Image, ImageOps, ImageFilter
 
 views = Flask(__name__)
 
+
 @views.route("/index", methods=["GET", "POST"])
 @views.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
 
-	if request.method == "POST":
+        image_change = request.form.get("edited")
 
-		image_change = request.form.get("edited")
+        if request.files:
+            # Check to see if there are any errors with the image
+            img = request.files["image"]
+            # Save the image to the Changed Images folder (Directory)
+            img.filename = str(uuid.uuid4()) + ".jpg"
+            img.save(os.path.join(config.Changed_Images, img.filename))
 
-		if request.files:
+        # Change the image
+        changed_image = apply_filter(img.filename, apply_filter)
 
-			# Check to see if there are any errors with the image
-			img = request.files["image"]
-			# Save the image to the Changed Images folder (Directory)
-			img.filename = str(uuid.uuid4()) + ".jpg"
-			img.save(os.path.join(config.Changed_Images, img.filename))
+        # Uploading to s3
+        upload(os.path.join(config.Changed_Images, changed_image), changed_image)
 
-		# Change the image
-		changedImage = applyFilter(img.filename, applyFilter())
+        # erase temp images, reset form
 
-		# Uploading to s3
-		upload(os.path.join(config.Changed_Images, changedImage), changedImage)
+        # Load all saved and edited images from s3
+        get_images_from_s3()
 
-		# erase temp images, reset form
+    return render_template('index.html')
 
-		# Load all saved and edited images from s3
-		get_images_from_s3()
-
-	return render_template('index.html')
 
 def get_images_from_s3():
+    # Make a request to the s3, which will get a list of all files in the bucket
+    response = list_content()
 
-	# Make a request to the s3, which will get a list of all files in the bucket
-	response = list_content()
+    # Get the url for each file pulled
+    images = []
+    for item in response:
+        images.append(config.base_s3_url + item["Key"])
 
-	# Get the url for each file pulled
-	images = []
-	for item in response:
-		images.append(config.base_s3_url + item["Key"])
-
-	# Inject that specific url into the html img tag
-	return render_template('index.html', photo_urls=images)
+    # Inject that specific url into the html img tag
+    return render_template('index.html', photo_urls=images)
 
 
-def applyFilter(filename, filter):
-
+def apply_filter(filename, filter_chosen):
     # Retrieve image here
     image = Image.open(os.path.join(config.Changed_Images, filename))
 
-    if filter == 'gray':
+    if filter_chosen == 'gray':
         im = ImageOps.grayscale(image)
-    elif filter == 'blur':
+    elif filter_chosen == 'blur':
         im = ImageOps.filter(ImageFilter.BLUR)
-    elif filter == 'solar':
+    elif filter_chosen == 'solar':
         im = ImageOps.solarize(image, threshold=80)
 
-	filtered_image = "filtered_" + filename
+        filtered_image = "filtered_" + filename
 
-	im.save(os.path.join(config.Changed_Images, filtered_image))
-	return filtered_image
+        im.save(os.path.join(config.Changed_Images, filtered_image))
+        return filtered_image
+
 
 if __name__ == "__main__":
-	views.run(debug=True)
+    views.run(debug=True)
